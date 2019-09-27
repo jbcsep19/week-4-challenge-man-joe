e37b449cd827261690d09352764d0034aff2d046
man-joe <jhoang3@gmail.com>
2019-09-27 03:27:47 -0400

    challenge complete

--- src/main/java/com/example/demo/BlogSiteAppController.java
+++ src/main/java/com/example/demo/BlogSiteAppController.java
@@ -7,6 +7,60 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BlogSiteAppController {
     @RequestMapping("/")
     public String home(){
-        return "/home";
+        return "home";
+    }
+    @RequestMapping("/about")
+    public String about(){
+        return "about";
+    }
+    @RequestMapping("/author")
+    public String author(){
+        return "author";
+    }
+
+
+    @RequestMapping("/github1")
+    public String gitHub1(){
+        return "github-p1";
+    }
+    @RequestMapping("/github2")
+    public String gitHub2(){
+        return "github-p2";
+    }
+
+    @RequestMapping("/program1")
+    public String program1(){
+        return "program1";
+    }
+    @RequestMapping("/program2")
+    public String program2(){
+        return "program2";
+    }
+
+    @RequestMapping("/core1")
+    public String core1(){
+        return "core1";
+    }
+    @RequestMapping("/core2")
+    public String core2(){
+        return "core2";
+    }
+
+    @RequestMapping("/ooj1")
+    public String ooj1(){
+        return "ooj1";
+    }
+    @RequestMapping("/ooj2")
+    public String ooj2(){
+        return "ooj2";
+    }
+
+    @RequestMapping("/html1")
+    public String html1(){
+        return "html1";
+    }
+    @RequestMapping("/html2")
+    public String html2(){
+        return "html2";
     }
 }
